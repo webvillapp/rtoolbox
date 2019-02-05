0192ae49488eaf415eb31c1be74c9ee36fa2cccb
wva <wva@lorem.pl>
2019-02-05 16:41:58 +0100

    Updates

---
+++ precache-manifest.a35b1007b951f69d05b7d614bf9944b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aa135224af2c558c28efc5a2f1e3985",
+    "url": "/webvillapp/rtoolbox/static/media/logo.3aa13522.svg"
+  },
+  {
+    "revision": "bd87017add99184088944831fb541a20",
+    "url": "/webvillapp/rtoolbox/static/media/bg.bd87017a.jpg"
+  },
+  {
+    "revision": "f748bfad4ecca8eeb82f",
+    "url": "/webvillapp/rtoolbox/static/js/runtime~main.f748bfad.js"
+  },
+  {
+    "revision": "c47008fe6134cebfe273",
+    "url": "/webvillapp/rtoolbox/static/js/main.c47008fe.chunk.js"
+  },
+  {
+    "revision": "13f3caa75a1acf5d53f9",
+    "url": "/webvillapp/rtoolbox/static/js/1.13f3caa7.chunk.js"
+  },
+  {
+    "revision": "c47008fe6134cebfe273",
+    "url": "/webvillapp/rtoolbox/static/css/main.30eb6c51.chunk.css"
+  },
+  {
+    "revision": "d79421803dd7b13f08decc18424e9b40",
+    "url": "/webvillapp/rtoolbox/index.html"
+  }
+];
